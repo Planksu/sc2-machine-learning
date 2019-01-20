012edeedaa0d33a436f5f5e96be0323715fba847
Antton Jokinen <antton.jokinen@gmail.com>
2019-01-20 13:00:32 +0200

    Fix setup.py to install packages correctly

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 import subprocess
 import sys
 
-packages = ['sc2', 'random', 'cv2', 'os', 'time', 'numpy', 'math', 'tensorflow', 'keras']
-modules = []
+modules = ['sc2', 'random', 'cv2', 'os', 'time', 'numpy', 'math', 'tensorflow', 'keras']
+ready_modules = []
 
-for i in packages:
+def install(package):
+	subprocess.call([sys.executable, "-m", "pip", "install", package])
+
+for i in modules:
 	try:
-		modules.append(__import__(i))
+		ready_modules.append(__import__(i))
 		print("Succesfully imported {}".format(i))
 	except ImportError as e:
 		print("Error importing {}".format(i))
-		install(i)
-
-
-async def install(package):
-	subprocess.call([sys.executable, "-m", "pip", "install", package])
+		if(i == 'cv2'):
+			install('opencv-python')
+		else:
+			install(i)
